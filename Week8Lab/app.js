45ea6113947a3ef894d88dff55b2802872011997
KCasey94 <x24164739@student.ncirl.ie>
2024-11-13 19:32:29 +0000

    Update app.js

--- Week8Lab/app.js
+++ Week8Lab/app.js
@@ -4,42 +4,42 @@ const game=() => {
         const paperB =document.querySelector('.paper');
         const scissorsB =document.querySelector('.scissors');
         const userOps = [rockB, paperB, scissorsB];
-        const gameOps = ['rock', 'paper', 'scissors']
+        const computerOps = ['rock', 'paper', 'scissors']
 
         userOps.forEach(option => {
             option.addEventListener('click',function(){
                 const pickNum = Math.random()*3;
-                const compPick = gameOps[pickNum];
+                const compPick = computerOps[pickNum];
 
                 winner(this.innerText, compPick)
             })
         })
     }
 
-    const winner = (user, game) => {
+    const winner = (user, computer) => {
         const result = document.querySelector('.result');
-        if(user === game){
+        if(user === computer){
             result='Tie';
         }
         else if(user =='rock'){
-            if(game =='paper'){
-                result='The game chose paper. You lose!'; 
+            if(computer =='paper'){
+                result='The computer chose paper. You lose!'; 
             }else{
-                result='The game chose scissors. You won!';
+                result='The computer chose scissors. You won!';
             }
         }
         else if(user =='paper'){
-            if(game =='scissors'){
-                result='The game chose scissors. You lose!'; 
+            if(computer =='scissors'){
+                result='The computer chose scissors. You lose!'; 
             }else{
-                result='The game chose rock. You won!';
+                result='The computer chose rock. You won!';
             }
         }
         else if(user =='scissors'){
-            if(game =='rock'){
-                result='The game chose rock. You lose!'; 
+            if(computer =='rock'){
+                result='The computer chose rock. You lose!'; 
             }else{
-                result='The game chose paper. You won!';
+                result='The computer chose paper. You won!';
             }
         }
 
